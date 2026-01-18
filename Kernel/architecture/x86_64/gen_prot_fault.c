98cd255d806d879d138e4106eeb569e231a0b636
Dev Sharma <devsharma.3624@gmail.com>
2026-01-18 11:51:29 +0000

    fix: changed general protection fault format specifiers to print larger addresses (higher half kernel values)

--- Kernel/architecture/x86_64/gen_prot_fault.c
+++ Kernel/architecture/x86_64/gen_prot_fault.c
@@ -11,12 +11,12 @@ void gpf_handler(trap_frame_t* frame) {
     k_printf("!!! GENERAL PROTECTION FAULT (Vector 13) !!!\n");
     k_printf("------------------------------------------------\n");
     
-    k_printf("Error Code: %x\n", frame->error_code);
-    k_printf("RIP: %x  (Instruction that failed)\n", frame->rip);
-    k_printf("CS : %x\n", frame->cs);
-    k_printf("RFLAGS: %x\n", frame->r_flags);
-    k_printf("RSP: %x  (Stack Pointer at crash)\n", frame->rsp);
-    k_printf("SS : %x\n", frame->ss);
+    k_printf("Error Code: %p\n", frame->error_code);
+    k_printf("RIP: %p  (Instruction that failed)\n", frame->rip);
+    k_printf("CS : %p\n", frame->cs);
+    k_printf("RFLAGS: %p\n", frame->r_flags);
+    k_printf("RSP: %p  (Stack Pointer at crash)\n", frame->rsp);
+    k_printf("SS : %p\n", frame->ss);
 
     // 4. Halt the CPU forever
     k_printf("\nSystem Halted.\n");
