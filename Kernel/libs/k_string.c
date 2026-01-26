73773b8c94c489aaa073ce86d4b4fb117c1580cf
Dev Sharma <devsharma.3624@gmail.com>
2026-01-26 10:06:02 +0000

    implemented kstrncmp to match only a limited starting characters of two strings

--- Kernel/libs/k_string.c
+++ Kernel/libs/k_string.c
@@ -23,3 +23,16 @@ int k_strcmp(const char* s1, const char* s2) {
 
     return *(const unsigned char*)s1 - *(const unsigned char*)s2;
 }
+
+int k_strncmp(const char* s1, const char* s2, int n) {
+    while (n > 0 && *s1 && (*s1 == *s2)) {
+        s1++;
+        s2++;
+        n--;
+    }
+
+    // If we ran out of 'n', it's a match (up to that point)
+    if (n == 0) return 0;
+
+    return *(const unsigned char*)s1 - *(const unsigned char*)s2;
+}
